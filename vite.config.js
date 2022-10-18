2680f8c05d0d2ce92b0e42f350e6c8ecc7692088
omarjesusperezortiz <omarjesus.perezortiz@gmail.com>
2022-10-18 10:38:27 +0200

    init

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,18 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  // Carpeta donde creara el build
+  build: { outDir: 'docs' },
+  // Comprobamos si estamos en produccion y la base es el nombre del repositorio, si no raiz
+  // condicion ? 'nombre-repositorio' : '/'
+  // Operador ternario
+  // if (process.env.NODE_ENV === 'produccion') {
+  // base: 'produccion'
+  // } else {
+  // base: '/
+  //    }
+  // produccion seria github pages
+  // desarrollo seria yarn dev
+  base: process.env.NODE_ENV === 'production' ? '/final-project-ironhack/' : '/'
 })
