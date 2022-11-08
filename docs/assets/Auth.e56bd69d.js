fe9e1c249eb91a873cc2422a9dfa408ae4eddf2b
omarjesusperezortiz <omarjesus.perezortiz@gmail.com>
2022-11-08 19:22:47 +0100

    init2

---
+++ docs/assets/Auth.e56bd69d.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./_plugin-vue_export-helper.cdc0426e.js";import{r,o as t,c}from"./index.580d614a.js";const n={};function s(_,a){const o=r("router-view");return t(),c(o)}const p=e(n,[["render",s]]);export{p as default};
